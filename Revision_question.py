202cdc6da6481c1ac3222e8ccab171d929038331
1234window# <tankbhavik721>
2024-01-24 23:25:49 +0530

    how to calculate maximum number

---
+++ Revision_question.py
@@ -0,0 +1,18 @@
+#max between number
+#a = 10
+#b = 3
+#c = 200
+#d = 20
+#if(a>b and a>c and a>d):
+##    print(a,"maximum value")
+#elif(b>a and b>c and b>d):
+#    print(b,"maximum value")
+#elif(c>a and c>b and c>d):
+#    print(c,"maximum value")
+#else:
+#    print(d, "maximum number")
+
+
+#
+            
+            
